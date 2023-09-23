01dfcf765141db025d9a079cc4ac8e22135f0605
Jamie Francisco <0jamiefrancisco@gmail.com>
2023-09-23 12:49:13 -0700

    Define functions for given exercises

--- functions.js
+++ functions.js
@@ -4,29 +4,56 @@
 // Use console.log() to see the return value in the console.
 
 // 1: Write a function named greeting that returns a string with a general greeting. 
-
+function greeting() {
+    return "Hello, World";
+  }
+  console.log(greeting());
 
 // 2: Write a function named customGreeting that returns a greeting WITH a specific name.
-
+function customGreeting(name) {
+    return `Hello, ${name}`;
+  }
+  console.log(customGreeting("Jamie"));
+  
 
 // 3: Write a function named greetPerson that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+function greetPerson(firstName, middleName, lastName) {
+    return `Hello, ${firstName} ${middleName} ${lastName} `;
+  }
+  console.log(greetPerson("Jamie", "M", "Francisco"));
 
 // 4: Write a function named square that takes in one number, and returns the square of that number.
 // BONUS: Print a sentence that interpolates the return value of your square function.
 
-
+function square(num) {
+    return num * num;
+  }
+  console.log(`The square of 8 is ${square(8)}.`);
 // 5: Write a function named checkStock that satisfies the following interaction pattern:
 // Hint: You will only write one checkStock function that checks the quantity and then prints the corresponding statement.
 
-checkStock(4, "Coffee");
-// => "Coffee is stocked"
+// checkStock(4, "Coffee");
+// // => "Coffee is stocked"
 
-checkStock(3, "Tortillas");
-// => "Tortillas - running LOW"
+// checkStock(3, "Tortillas");
+// // => "Tortillas - running LOW"
 
-checkStock(0, "Cheese");
-// => "Cheese - OUT of stock!"
+// checkStock(0, "Cheese");
+// // => "Cheese - OUT of stock!"
 
-checkStock(1, "Salsa");
-// => "Salsa - running LOW"
+// checkStock(1, "Salsa");
+// // => "Salsa - running LOW"
+function checkStock(quantity, product) {
+    if (quantity >= 4) {
+      console.log(`${product} is stocked`);
+    } else if (quantity === 0) {
+      console.log(`${product} - OUT of stock!`);
+    } else {
+      console.log(`${product} - running LOW`);
+    }
+  }
+  checkStock(4, "Coffee");
+  checkStock(3, "Tortillas");
+  checkStock(0, "Cheese");
+  checkStock(1, "Salsa");
+  
